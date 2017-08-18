98eb2997727bbe259579079e4fe320be7b8df122
Hannah Brinsko <h.brinsko@utexas.edu>
2017-08-18 09:21:53 -0500

    Added background and scenario outline to search feature

--- features/search.feature
+++ features/search.feature
@@ -1,5 +1,12 @@
-Feature: Site Search
-Scenario:
-  Given We navigate to the google
-  When We search for the word Zendaya
-  Then The results for the search will be displayed
+Feature: Google searching
+Background:
+  Given We navigate to the google homepage
+Scenario Outline:
+  When We search for "<word>"
+  And We click on the images link
+  Then The results for the image search will be displayed
+Examples:
+|word|
+|Zendaya|
+|Donald Trump|
+|Pam and Jim|
--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,17 +1,23 @@
 require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :chrome
+wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
 
-Given(/^We navigate to the google$/) do
+Given(/^We navigate to the google homepage$/) do
   driver.navigate.to "http://www.google.com/"
 end
 
-When(/^We search for the word Zendaya$/) do
+When(/^We search for "(.*?)"$/) do |word|
   input = driver.find_element(:name, 'q')
-  input.send_keys('Zendaya')
+  input.send_keys(word)
   input.send_keys(:enter)
 end
 
-Then(/^The results for the search will be displayed$/) do
-  wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
+When(/^We click on the images link$/) do
   wait.until { driver.find_element(:id => "main") }
+  imageLink = driver.find_element(:link_text => "Images")
+  imageLink.click()
+end
+
+Then(/^The results for the image search will be displayed$/) do
+  wait.until { driver.find_element(:id => "rcnt") }
 end
